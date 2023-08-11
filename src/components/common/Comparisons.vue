02e27bc7cf1613c8af2783996c268d4f3f76031b
Manuel Mandujano <manduuu2294@gmail.com>
2023-08-10 19:43:31 -0500

    competitor comparison section update

--- src/components/common/Comparisons.vue
+++ src/components/common/Comparisons.vue
@@ -2,63 +2,50 @@
     <div class="comparisons-area ptb-70 bg-f7fafd">
         <div class="container">
             <div class="section-title">
-                <h2>Compare us with others</h2>
+                <h2>Compáranos en el mercado</h2>
                 <div class="bar"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Creamos experiencia diferente a las que ofrece el mercado, tipo de cambio justo, seguridad, atención rápida y personalizada, y sobre todo muy fácil de usar gracias a nuestra plataforma de atención por whatsapp.</p>
             </div>
 
             <div class="comparisons-table table-responsive">
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">Packages</th>
-                            <th scope="col">Freelancer</th>
-                            <th scope="col">Householder</th>
-                            <th scope="col">Business Man</th>
+                            <th scope="col">Cualidades</th>
+                            <th scope="col">Calle</th>
+                            <th scope="col">Bancos</th>
+                            <th scope="col">Casa Digitales</th>
+                            <th scope="col">Cambios JP</th>
                         </tr>
                     </thead>
                     
                     <tbody>
                         <tr>
-                            <td>Control payout timing</td>
+                            <td>Protección en todo momento</td>
+                            <td><i class="flaticon-cancel"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                         </tr>
                         <tr>
-                            <td>Transparent payouts</td>
-                            <td><i class="flaticon-check-mark"></i></td>
+                            <td>Transferencias rápidas</td>
                             <td><i class="flaticon-cancel"></i></td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Automate evidence submission</td>
-                            <td><i class="flaticon-cancel"></i></td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Collaboration notes</td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                            <td><i class="flaticon-cancel"></i></td>
-                            <td><i class="flaticon-cancel"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Deposit tagging</td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                            <td><i class="flaticon-check-mark"></i></td>
-                            <td><i class="flaticon-cancel"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Technical support over IRC</td>
                             <td><i class="flaticon-check-mark"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                         </tr>
                         <tr>
-                            <td>24×7 support</td>
+                            <td>Mejor tipo de cambio</td>
+                            <td><i class="flaticon-cancel"></i></td>
                             <td><i class="flaticon-cancel"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
+                            <td><i class="flaticon-check-mark"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Fácil uso</td>
+                            <td><i class="flaticon-cancel"></i></td>
+                            <td><i class="flaticon-cancel"></i></td>
+                            <td><i class="flaticon-cancel"></i></td>
                             <td><i class="flaticon-check-mark"></i></td>
                         </tr>
                     </tbody>
